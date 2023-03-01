5bb6ccb42bc6a37b90d13bbedfc9108fe8a0cd43
NurleliFitriyani <leliyani16desember@gmail.com>
2023-03-01 18:07:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nurlelify/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nurlelify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
